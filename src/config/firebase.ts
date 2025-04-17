66f5cab0d4a1565eead97a8550bb2c44d00618b2
Roman Dmytro <rc.dev0401@gmail.com>
2025-04-17 15:15:26 -0400

    remove data

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCnNrR3WArIZnrFlqf7kDzeMKmcUTfnWPg",
-  authDomain: "authentication-66a07.firebaseapp.com",
-  projectId: "authentication-66a07",
-  storageBucket: "authentication-66a07.appspot.com",
-  messagingSenderId: "427014431883",
-  appId: "1:427014431883:web:15deb554d7784e909ae08e",
-  measurementId: "G-1234567890",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
